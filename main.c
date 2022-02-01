9a0619f97b47a58c233723078ac3377ba9fd2314
Edivan Steri <edivansteri01@gmail.com>
2022-02-01 17:13:34 +0100

    merge printf di edivan e patrizio e marco

--- main.c
+++ main.c
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-int main(){
-    printf("Hello,world my name is MARCO");
-     printf("Hello,world");
-    return 0;
-}
-
-=======
 #include <stdio.h>
 int main ()
 {
     printf("Hello World!");
     printf("Esshperienza!!!");
     printf("Patrizio");
+    printf("Hello,world my name is MARCO");
 }
->>>>>>> b148e51ad07dca7e45bb5994beb3c7431e72f0a9
+
